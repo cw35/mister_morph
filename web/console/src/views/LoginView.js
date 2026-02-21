72d1feb934c57f92058f7e3dee724545761aed4d
Lyric <5h3ll3x@gmail.com>
2026-02-21 12:39:49 +0900

    mod

--- web/console/src/views/LoginView.js
+++ web/console/src/views/LoginView.js
@@ -48,9 +48,17 @@ const LoginView = {
   },
   template: `
     <section class="login-box">
-      <h1 class="login-title">Mistermorph Console</h1>
-      <div class="login-language">
-        <QLanguageSelector :lang="lang" :presist="true" @change="onLanguageChange" />
+      <div class="login-brand">
+        <span class="login-brand-mark" aria-hidden="true">
+          <svg class="login-brand-logo" viewBox="0 0 24 24" role="presentation">
+            <path d="M3 11h18" />
+            <path d="M5 11V7a3 3 0 0 1 3-3h8a3 3 0 0 1 3 3v4" />
+            <path d="M7 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0-6 0" />
+            <path d="M17 17m-3 0a3 3 0 1 0 6 0a3 3 0 1 0-6 0" />
+            <path d="M10 17h4" />
+          </svg>
+        </span>
+        <h1 class="login-title">Mister Morph Console</h1>
       </div>
       <form class="stack" @submit.prevent="submit">
         <QInput
@@ -63,6 +71,12 @@ const LoginView = {
         <QButton :loading="busy" class="primary" @click="submit">{{ t("login_button") }}</QButton>
         <QFence v-if="err" type="danger" icon="QIconCloseCircle" :text="err" />
       </form>
+      <div class="login-footer">
+        <div class="login-divider" aria-hidden="true"></div>
+        <div class="login-language">
+          <QLanguageSelector :lang="lang" :presist="true" @change="onLanguageChange" />
+        </div>
+      </div>
     </section>
   `,
 };
